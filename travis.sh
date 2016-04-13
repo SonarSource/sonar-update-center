2c991329563a10b3ad0296d48eb27abc0ef2ff8b
Tom <thomas.verin@sonarsource.com>
2016-04-13 17:34:19 +0200

    Setup QA

--- travis.sh
+++ travis.sh
@@ -1,61 +1,12 @@
 #!/bin/bash
 
-set -euo pipefail
+#set -euo pipefail
 
-function installTravisTools {
-  mkdir ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v21 | tar zx --strip-components 1 -C ~/.local
+function configureTravis {
+  mkdir -p ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v27 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
+configureTravis
 
-
-function strongEcho {
-  echo ""
-  echo "================ $1 ================="
-}
-
-installTravisTools
-
-if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  strongEcho 'Build and analyze commit in master'
-
-  # integration of jacoco report is quite memory-consuming
-  export MAVEN_OPTS="-Xmx1G -Xms128m"
-
-  # this commit is master must be built and analyzed (with upload of report)
-  mvn -B -e -V clean org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar \
-    -Pcoverage-per-test \
-    -Dmaven.test.redirectTestOutputToFile=false \
-    -Dsonar.host.url=$SONAR_HOST_URL \
-    -Dsonar.login=$SONAR_TOKEN
-
-elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "$GITHUB_TOKEN" ]; then
-  # For security reasons environment variables are not available on the pull requests
-  # coming from outside repositories
-  # http://docs.travis-ci.com/user/pull-requests/#Security-Restrictions-when-testing-Pull-Requests
-  # That's why the analysis does not need to be executed if the variable GITHUB_TOKEN is not defined.
-
-  # integration of jacoco report is quite memory-consuming
-  export MAVEN_OPTS="-Xmx1G -Xms128m"
-
-  strongEcho 'Build and analyze pull request'
-  # this pull request must be built and analyzed (without upload of report)
-  mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar \
-    -Pcoverage-per-test \
-    -Dmaven.test.redirectTestOutputToFile=false \
-    -Dsonar.analysis.mode=issues \
-    -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
-    -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
-    -Dsonar.github.oauth=$GITHUB_TOKEN \
-    -Dsonar.host.url=$SONAR_HOST_URL \
-    -Dsonar.login=$SONAR_TOKEN \
-    -B -e -V
-
-else
-  strongEcho 'Build, no analysis'
-  # Build branch, without any analysis
-
-  # No need for Maven goal "install" as the generated JAR file does not need to be installed
-  # in Maven local repository
-  mvn verify -Dmaven.test.redirectTestOutputToFile=false -B -e -V
-fi
+regular_mvn_build_deploy_analyze
