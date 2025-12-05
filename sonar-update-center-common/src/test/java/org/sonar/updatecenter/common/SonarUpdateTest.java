bd103385b6d9cb9a14a6fc245a6ce3aff900d83c
Lukasz Jarocki <lukasz.jarocki@sonarsource.com>
2025-12-05 10:41:31 +0100

    UPC-163 Update license headers

--- sonar-update-center-common/src/test/java/org/sonar/updatecenter/common/SonarUpdateTest.java
+++ sonar-update-center-common/src/test/java/org/sonar/updatecenter/common/SonarUpdateTest.java
@@ -1,6 +1,6 @@
 /*
  * SonarSource :: Update Center :: Common
- * Copyright (C) 2010-2025 SonarSource SA
+ * Copyright (C) 2010-2025 SonarSource Sàrl
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
